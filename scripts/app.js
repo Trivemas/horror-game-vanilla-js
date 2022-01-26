51133035d5dc1eb8366d6265cee716a65c8d58dc
Tyler Barnett <tylerjbarnett1411@gmail.com>
2022-01-26 16:25:04 -0700

    Player class, gameloop, keystroke registration.

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,40 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+class Player {
+	constructor() {
+		this.width = 32;
+		this.height = 32;
+		this.x = canvas.width / 2 - this.width / 2;
+		this.y = canvas.height / 2 - this.height / 2;
+
+		this.wireUpEvents();
+	}
+	update() {}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+	wireUpEvents() {
+		window.addEventListener("keydown", (e) => {
+			console.log(e.key);
+		});
+	}
+}
+
+let player = new Player();
+
+function gameloop() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height); /* clear canvas */
+
+	player.update();
+	player.render();
+
+	requestAnimationFrame(gameloop);
+}
+
+requestAnimationFrame(gameloop);
